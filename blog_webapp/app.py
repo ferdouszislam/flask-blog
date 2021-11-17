136cdd1155314cdd88505113478a933c2a9e1d59
ferdouszislam <ferdouszislam@gmail.com>
2021-11-17 18:53:53 +0600

    Created database with sqlalchemy and sqlite
    - User and Post model classes created
    - database uri set to sqlite for development

--- blog_webapp/app.py
+++ blog_webapp/app.py
@@ -1,11 +1,53 @@
+from datetime import datetime
 from flask import Flask, escape, request, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 from utils import dummy_data
 
+# config vars
 app = Flask(__name__)
+# secret key for security (used in form html templates)
 app.config['SECRET_KEY'] = '45ff14277abe5e2feed981b91e21cb5a'
+# database url, using sqlite for development
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogsite.db'
+db = SQLAlchemy(app)
 
 
+# models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    # string length 20 for hashing image files,
+    # so that file names are unique
+    profile_image_file = db.Column(db.String(20), nullable=False,
+                                   default='default_profile_image.jpg')
+    # string length 60 for hashing
+    password = db.Column(db.String(60), nullable=False)
+
+    # one user can have many posts
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        # python's toString()
+        return f"User('{self.username}', '{self.email}', '{self.profile_image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    # all posts have a user as author
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        # python's toString()
+        return f"Post('{self.title}', '{self.timestamp}')"
+
+
+# apis
 @app.route('/')
 @app.route('/home')
 def home():
