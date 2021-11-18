b72c77e79e8fb9c5bd46306ba13188e079c2ec7e
ferdouszislam <ferdouszislam@gmail.com>
2021-11-18 06:00:46 +0600

    added update and delete post functionality

--- blog_webapp/blog_webapp/routes.py
+++ blog_webapp/blog_webapp/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from blog_webapp import app, db, bcrypt
 from blog_webapp.forms import RegistrationForm, LoginForm, UpdateProfileForm, PostForm
 from blog_webapp.utils import dummy_data
@@ -171,7 +171,8 @@ def create_post():
         # but the last return makes a POST request making a form resubmit
         return redirect(url_for('home'))
 
-    return render_template('create_post.html', title='Create Post', form=form)
+    return render_template('create_or_edit_post.html', title='Create Post',
+                           form=form, legend='Create Post')
 
 
 @app.route("/post/<int:post_id>")
@@ -179,3 +180,63 @@ def get_post(post_id):
     # get_or_404() signals 404 Not Found in the browser automatically if data does not exist
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    # get_or_404() signals 404 Not Found in the browser automatically if data does not exist
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        info_updated = False
+
+        if form.title.data != post.title:
+            post.title = form.title.data
+            info_updated = True
+
+        if form.content.data != post.content:
+            post.content = form.content.data
+            info_updated = True
+
+        if info_updated:
+            # update database
+            db.session.commit()
+
+        # let user know
+        flash('Your post was updated!', 'success')
+
+        # don't let it fall to the return at the end
+        # redirect makes a GET request
+        # but the last return makes a POST request making a form resubmit
+        return redirect(url_for('get_post', post_id=post.id))
+
+    elif request.method == 'GET':
+        # GET request means user navigated to this page and did not submit form
+        # populate the form with existing data
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('create_or_edit_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    # get_or_404() signals 404 Not Found in the browser automatically if data does not exist
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    # delete from database
+    db.session.delete(post)
+    db.session.commit()
+
+    # let user know
+    flash('The post was deleted!', 'success')
+
+    return redirect(url_for('home'))
